146bce6750173043c2d575e35b71f7dcfe921dff
HyunKoun Kim(Tony) <53275791+Tony-Kim09@users.noreply.github.com>
2020-08-28 14:12:37 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
             <meta name='viewport' content='width=device-width, initial-scale=1'>
             <link rel='stylesheet' href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
             <link rel='stylesheet' href='style.css'>
-            <script src='OSC.js'></script>
             <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
             <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> 
     _INIT;
